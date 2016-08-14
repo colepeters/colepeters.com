31738a162210635a4633193880dce7b590d4be91
Cole Peters <cole@colepeters.com>
2016-08-14 22:42:38 +0100

    Add initial work index and 32 Frames placeholder

--- tasks/copy.js
+++ tasks/copy.js
@@ -2,6 +2,6 @@ const gulp = require('gulp');
 
 gulp.task('copy', () => {
   return gulp
-    .src(['src/images/*.jpg'])
+    .src(['src/images/*.jpg', 'src/images/*.png'])
     .pipe(gulp.dest('public/images'));
 });
