5e9ad67fd4f015b133e7c39437324190108aca17
Cole Peters <cole@colepeters.com>
2016-10-28 20:20:44 +0100

    🔥 BLAM! Clean slate

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const runSequence = require('run-sequence');
-
-require('require-dir')('./tasks');
-
-gulp.task('default', done => {
-  runSequence([ 'templates', 'style'], done);
-});
--- tasks/copy.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-
-gulp.task('copy', () => {
-  return gulp
-    .src(['src/images/*.jpg', 'src/images/*.png'])
-    .pipe(gulp.dest('public/images'));
-});
