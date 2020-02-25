efdbec64f8d09797555479ad389566718e584b2e
Cole Peters <cole@colepeters.com>
2020-02-24 20:24:22 -0600

    Cleanup dm sans import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import App from 'next/app'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import 'typeface-dm-sans'
 
 import GlobalStyle from '../components/GlobalStyle'
 import theme from '../theme'
