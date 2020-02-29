c243bb38371278f003fc93cca344dc113e5e5354
Cole Peters <cole@colepeters.com>
2020-02-28 18:17:42 -0600

    Build NavLink, add subpages, Motion component

---
+++ components/NavLink.js
@@ -0,0 +1,18 @@
+import Link from 'next/Link'
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+
+const CustomLink = styled.a`
+  font-weight: ${props => (props.pathname === props.href ? '700' : '400')};
+`
+CustomLink.displayName = 'CustomLink'
+
+export default function NavLink({ children, href }) {
+  const { pathname } = useRouter()
+
+  return (
+    <Link href={href} passHref>
+      <CustomLink pathname={pathname}>{children}</CustomLink>
+    </Link>
+  )
+}
--- components/index.js
+++ components/index.js
@@ -1,8 +1,10 @@
 export { default as Box } from './primitives/Box'
-
+export { default as MotionBox } from './primitives/MotionBox'
 export { default as Heading } from './primitives/Heading'
 export { default as Text } from './primitives/Text'
 export { default as List } from './primitives/List'
+
+export { default as NavLink } from './NavLink'
 
 export { Container } from './layout'
 export { default as Header } from './Header'
