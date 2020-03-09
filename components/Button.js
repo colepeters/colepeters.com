a907acc1adb58a49a756ddac144ba4beed5ca8ef
Cole Peters <cole@colepeters.com>
2020-03-08 22:52:10 -0500

    Stub Button, adjust nav animation

---
+++ components/Button.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  appearance: none;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-weight: 500;
+  letter-spacing: 0.1em;
+  padding: 0;
+  text-transform: uppercase;
+`
+Button.displayName = 'Button'
+
+export default Button
