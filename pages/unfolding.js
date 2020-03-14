b9ca05e3e3c50ecd7830171c5fbe310d0e5b7dc1
Cole Peters <cole@colepeters.com>
2020-03-14 13:19:10 -0500

    Write Unfolding bio

--- pages/unfolding.js
+++ pages/unfolding.js
@@ -1,5 +1,3 @@
-import Link from 'next/Link'
-
 import { Container, Header, Heading, Text } from '../components'
 
 export default function Unfolding() {
@@ -7,25 +5,14 @@ export default function Unfolding() {
     <>
       <Header />
       <Container as='section'>
-        <Heading
-          as='h2'
-          fontWeight='400'
-          fontSize={5}
-          letterSpacing='0.05em'
-          style={{ textTransform: 'uppercase' }}
-        >
+        <Heading fontWeight='300' mb={[4, null, 5]}>
           Unfolding
         </Heading>
         <Text>
-          As a sound artist, I work within the vein of musique concrète, noise,
-          drone, and other non-traditional and experimental forms of expression.
-          The physicality and experiential effects of sound, and the
-          interpretation of the broader physical world through this medium, are
-          the primary fascinations I explore with my project{' '}
-          <Link href='/unfolding'>
-            <a>Unfolding</a>
-          </Link>
-          .
+          Unfolding is a project exploring interpretations of the physical world
+          through the medium of sound. The work is informed by a diverse range
+          of inputs, from classical and quantum physics to the sheer physicality
+          of auditory stimuli.
         </Text>
       </Container>
     </>
