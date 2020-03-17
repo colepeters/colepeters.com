5bf889bbb2a51b17341d45e9a00b2a4a9308c818
Cole Peters <cole@colepeters.com>
2020-03-16 23:32:37 -0500

    Release, Tracklist, typography, etc

---
+++ components/releases/Tracklist.js
@@ -0,0 +1,38 @@
+import { List, Text, Box } from '../index'
+
+export default function Tracklist({ tracks }) {
+  return (
+    <List reset as='ol' pl={0} my={3}>
+      {tracks.map(({ name, length }, i) => (
+        <Text as='li' key={name}>
+          <Box display='inline-flex' width={1}>
+            <Text as='span' width={['50%', null, '66%']}>
+              <Text
+                as='span'
+                display='inline-block'
+                color='muted'
+                fontSize={0}
+                width='2ch'
+                style={{
+                  fontVariantNumeric: 'tabular-nums',
+                }}
+              >
+                {i + 1}.
+              </Text>{' '}
+              {name}
+            </Text>
+            <Text
+              as='span'
+              color='muted'
+              style={{
+                fontVariantNumeric: 'tabular-nums slashed-zero',
+              }}
+            >
+              {length}
+            </Text>
+          </Box>
+        </Text>
+      ))}
+    </List>
+  )
+}
