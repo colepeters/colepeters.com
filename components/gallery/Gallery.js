108af9bff60a0abe5912729669d5a4b09e0cbc83
Cole Peters <cole@colepeters.com>
2020-03-28 16:45:01 -0500

    Make gallery images zoomable

--- components/gallery/Gallery.js
+++ components/gallery/Gallery.js
@@ -1,18 +1,20 @@
 import { Heading, RichText, Box, Text } from '../index'
 
+import GalleryImage from './GalleryImage'
+
 export default function Gallery({ gallery }) {
   return (
     <>
       <Heading mb={[4, 5]}>{gallery.title}</Heading>
       {gallery.description && <RichText render={gallery.description} />}
 
+      <Text fontSize={0} color='muted' mt={4} fontStyle='italic'>
+        Tap images to zoom in; tap again to zoom out.
+      </Text>
+
       {gallery.entries.map(e => (
         <Box as='figure' key={e.image.alt} mx={0} mt={[5, 6]}>
-          <img
-            src={e.image.url}
-            alt={e.image.alt}
-            style={{ maxWidth: '100%', maxHeight: '95vh' }}
-          />
+          <GalleryImage src={e.image.url} alt={e.image.alt} />
           <Text as='figcaption'>
             <Text>{e.name}</Text>
             <Box fontSize={0} color='muted'>
---
+++ components/gallery/GalleryImage.js
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from 'react'
+
+import usePrevious from '../../hooks/usePrevious'
+import { Box } from '../index'
+
+export default function GalleryImage({ src, alt, height, width }) {
+  const imgRef = useRef(null)
+  const [isZoomed, setIsZoomed] = useState(false)
+  const toggleZoom = () => setIsZoomed(prev => !prev)
+
+  const prevZoom = usePrevious(isZoomed)
+
+  useEffect(() => {
+    if (prevZoom && !isZoomed) {
+      window.scrollTo({
+        top: imgRef?.current.offsetTop + 10,
+        left: 0,
+      })
+    }
+  }, [isZoomed])
+
+  return (
+    <Box
+      onClick={toggleZoom}
+      width='100%'
+      style={{
+        cursor: isZoomed ? 'zoom-out' : 'zoom-in',
+      }}
+    >
+      <img
+        src={src}
+        alt={alt}
+        ref={imgRef}
+        style={{
+          maxHeight: isZoomed ? '225vh' : '95vh',
+          maxWidth: isZoomed ? '150vw' : '100%',
+        }}
+      />
+    </Box>
+  )
+}
