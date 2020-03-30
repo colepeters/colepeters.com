701d2a15732c65bce2e9c1ed13363c1a07d1aa7d
Cole Peters <cole@colepeters.com>
2020-03-29 23:36:10 -0500

    Add presentation transports

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components'
+import {
+  color,
+  compose,
+  display,
+  layout,
+  opacity,
+  position,
+  space,
+  typography,
+} from 'styled-system'
 
 const Button = styled.button`
+  ${compose(color, display, layout, opacity, position, space, typography)}
   appearance: none;
   background: transparent;
   border: none;
   cursor: pointer;
+  display: inline-block;
   font-weight: 500;
   padding: 0;
   text-decoration: underline;
@@ -12,6 +24,10 @@ const Button = styled.button`
   &:focus {
     outline: none;
   }
+
+  transition: opacity 0.5s ease;
+
+  ${props => props.disabled && 'opacity: 0.5; pointer-events: none;'}
 `
 Button.displayName = 'Button'
 
