0084d0bcc0314953830f8f55c3a93774f4e51973
Cole Peters <cole@colepeters.com>
2020-03-31 18:46:02 -0500

    Add IPR, show year on Presentation

--- components/Presentation.js
+++ components/Presentation.js
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { Box, Button, Heading, RichText, Text } from './index'
 
 export default function Presentation({ presentation }) {
-  const { title, description, entries } = presentation
+  const { title, description, entries, year } = presentation
 
   const [index, setIndex] = useState(0)
   const inc = () => setIndex(prev => prev + 1)
@@ -15,15 +15,24 @@ export default function Presentation({ presentation }) {
 
   return (
     <>
-      <Heading mb={[4, 5]} position='relative' left={[0, null, '-4px']}>
+      <Heading
+        mb={year ? 3 : [4, 5]}
+        position='relative'
+        left={[0, null, '-4px']}
+      >
         {title}
       </Heading>
+      {year && (
+        <Text color='muted' mb={[4, 5]}>
+          {year}
+        </Text>
+      )}
 
       {description && <RichText render={description} />}
 
       <Box my={[4, 5]}>
         <Box key={current.image.url} as='figure' m={0}>
-          <img src={current.image.url} />
+          <img src={current.image.url} style={{ maxHeight: '75vh' }} />
           <figcaption>
             <Text fontSize={0} my={3}>
               {current.caption}
---
+++ pages/images-pour-rien.js
@@ -0,0 +1,28 @@
+import Presentation from '../components/Presentation'
+import setTitle from '../utils/setTitle'
+import { Client } from '../prismic-configuration'
+import { Container, Box, Header, Heading, RichText, Text } from '../components'
+
+export async function getStaticProps() {
+  const { data: presentation } = await Client().getByUID(
+    'presentation',
+    'images-pour-rien'
+  )
+  return {
+    props: {
+      presentation,
+    },
+  }
+}
+
+export default function Locus({ presentation }) {
+  return (
+    <>
+      {setTitle('Locus')}
+      <Header />
+      <Container as='section'>
+        <Presentation presentation={presentation} />
+      </Container>
+    </>
+  )
+}
