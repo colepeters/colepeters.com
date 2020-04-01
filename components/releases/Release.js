1e9533a9368b881168dbf5bfdaf08ff52de69140
Cole Peters <cole@colepeters.com>
2020-03-31 22:55:32 -0500

    Iconate the things

--- components/releases/Release.js
+++ components/releases/Release.js
@@ -1,22 +1,45 @@
+import { IoMdPlay } from 'react-icons/io'
+
 import Tracklist from './Tracklist'
 import { Box, Heading, RichText, Text } from '../index'
 
 export default function Release({ release, ...props }) {
+  const {
+    title,
+    release_date,
+    format,
+    tracks,
+    description,
+    cover,
+    link,
+  } = release.data
+
   return (
     <Box as='article' display='flex' flexWrap='wrap' {...props}>
       <Box width={['100%', null, '50%']} order={[2, null, 1]}>
         <Box pr={[0, null, 5]}>
           <Heading as='h2' mb={3}>
-            {release.data.title}
+            {title}
           </Heading>
-          <Text color='muted'>
-            {release.data.release_date.split('-').join('/')}
-          </Text>
+          <Text color='muted'>{release_date.split('-').join('/')}</Text>
           <Text mt={0} color='muted'>
-            {release.data.format}
+            {format}
           </Text>
-          <Tracklist tracks={release.data.tracks} />
-          <RichText render={release.data.description} />
+          <Tracklist tracks={tracks} />
+          <Text fontWeight={500} my={4}>
+            <a href={link}>
+              <Text
+                as='span'
+                pr={2}
+                textDecoration='none'
+                verticalAlign='middle'
+              >
+                <IoMdPlay />
+              </Text>
+              Stream and purchase on Bandcamp
+            </a>
+          </Text>
+          <RichText render={description} />
         </Box>
       </Box>
 
@@ -29,7 +52,7 @@ export default function Release({ release, ...props }) {
           maxWidth={['35ch', null, 'none']}
           boxShadow='0 4px 32px rgba(0,0,0,0.075), 0 2px 6px rgba(0,0,0,0.05)'
         >
-          <img src={release.data.cover.url} />
+          <img src={cover.url} />
         </Box>
       </Box>
     </Box>
