e8b27b938a0feb8cb36d58ff069889b98821b362
Cole Peters <cole@colepeters.com>
2020-04-06 00:35:31 -0500

    Add contact form & email api

---
+++ components/Input.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+import { Text } from './index'
+
+const StyledInput = styled.input`
+  appearance: none;
+  background: rgba(0, 0, 0, 0.05);
+  border: none;
+  border-bottom: 1px solid black;
+  display: block;
+  line-height: ${props => props.theme.lineHeights.body};
+  padding: ${props => props.theme.space[2]};
+  position: relative;
+  margin: ${props => props.theme.space[1]} 0 ${props => props.theme.space[3]} 0;
+  transition: background 0.3s ease;
+  width: 100%;
+
+  &:focus {
+    background: rgba(0, 0, 0, 0.025);
+    outline: none;
+  }
+`
+
+StyledInput.displayName = 'Input'
+
+export default function Input({
+  label,
+  required = true,
+  type = 'text',
+  ...props
+}) {
+  return (
+    <Text as='label'>
+      <Text as='span' fontWeight='500'>
+        {label}
+      </Text>{' '}
+      {required && (
+        <Text as='span' color='muted'>
+          *
+        </Text>
+      )}
+      <StyledInput type={type} {...props} />
+    </Text>
+  )
+}
---
+++ components/Textarea.js
@@ -0,0 +1,42 @@
+import TextareaAutosize from 'react-autosize-textarea'
+import styled from 'styled-components'
+
+import { Text } from './index'
+
+const StyledTextarea = styled(TextareaAutosize)`
+  appearance: none;
+  background: rgba(0, 0, 0, 0.05);
+  border: none;
+  border-bottom: 1px solid black;
+  display: block;
+  line-height: ${props => props.theme.lineHeights.body};
+  padding: ${props => props.theme.space[2]};
+  position: relative;
+  margin: ${props => props.theme.space[1]} 0 ${props => props.theme.space[3]} 0;
+  resize: vertical;
+  transition: background 0.3s ease;
+  width: 100%;
+
+  &:focus {
+    background: rgba(0, 0, 0, 0.025);
+    outline: none;
+  }
+`
+
+StyledTextarea.displayName = 'Input'
+
+export default function Textarea({ label, required = true, ...props }) {
+  return (
+    <Text as='label'>
+      <Text as='span' fontWeight='500'>
+        {label}
+      </Text>{' '}
+      {required && (
+        <Text as='span' color='muted'>
+          *
+        </Text>
+      )}
+      <StyledTextarea async rows={4} {...props} />
+    </Text>
+  )
+}
