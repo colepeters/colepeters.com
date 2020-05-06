bfe95027315034d76472fc557553bc5f316f1f47
Cole Peters <cole@colepeters.com>
2020-05-05 22:18:03 -0500

    Migrate Releases to Contentful

--- components/releases/Tracklist.js
+++ components/releases/Tracklist.js
@@ -3,36 +3,21 @@ import { List, Text, Box } from '../index'
 export default function Tracklist({ tracks }) {
   return (
     <List reset as='ol' pl={0} my={4}>
-      {tracks.map(({ name, length }, i) => (
-        <Text as='li' key={name}>
-          <Box display='inline-flex' width={1}>
-            <Text as='span' width={['50%', null, '66%']}>
-              <Text
-                as='span'
-                display='inline-block'
-                color='muted'
-                fontSize={0}
-                width='2ch'
-                style={{
-                  fontVariantNumeric: 'tabular-nums',
-                }}
-              >
-                {i + 1}.
-              </Text>{' '}
-              {name}
-            </Text>
-            <Text
-              as='span'
-              color='muted'
-              fontSize={0}
-              lineHeight='1.6rem'
-              style={{
-                fontVariantNumeric: 'tabular-nums slashed-zero',
-              }}
-            >
-              {length}
-            </Text>
-          </Box>
+      {tracks.map((t, i) => (
+        <Text as='li' key={t}>
+          <Text
+            as='span'
+            display='inline-block'
+            color='muted'
+            fontSize={0}
+            width='2ch'
+            style={{
+              fontVariantNumeric: 'tabular-nums',
+            }}
+          >
+            {i + 1}.
+          </Text>{' '}
+          {t}
         </Text>
       ))}
     </List>
